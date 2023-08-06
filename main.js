77f2f704a94716e9e1998d9b3912c65b2f2ba419
Wyatt <wyattstephenson@rocketmail.com>
2023-08-05 18:00:14 -0600

    1 BUILDING SIMPLE LIKER

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const userToLike = document.getElementsByClassName("like-glyph")
+const updateLike = userToLike[0]
 
+updateLike.addEventListener("click", (e)=>{
 
+mimicServerCall()
+.then((r)=>{
+  //   When the "server" returns a success status:
+// Change the heart to a full heart
 
+updateLike.classList.add("activated-heart")
+//added the full heart to make it more visually appealing
+updateLike.innerText = FULL_HEART
+// updateLike.innerHTML = ""
+
+})
+.catch((error)=>{
+  const pageError = document.querySelector("h2")
+  const modal = document.getElementById("modal")
+
+  pageError.classList.add("")
+  // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+
+  modal.innerContent = error
+  setTimeout(modal.id = ".hidden")
+  
+//closes the server call chain, the last of which is the .catch
+})
+//closes the event listener
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
